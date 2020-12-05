d2f68ef9adf97191ae662a155c6ff14b042e8ab9
Nick Pedersen <admin@nickpedersen.com>
2020-12-05 19:29:35 +1100

    Day 5

---
+++ day5-part1.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const puzzleInput = fs
+  .readFileSync(__dirname + "/day5input.txt")
+  .toString()
+  .split("\n");
+
+const binaryPartition = (str, upperKey) => {
+  const max = Math.pow(2, str.length);
+  let range = [0, max];
+  str.split("").forEach((value) => {
+    const remainingSize = range[1] - range[0];
+    if (value === upperKey) {
+      range[0] = range[1] - remainingSize / 2;
+    } else {
+      range[1] = range[0] + remainingSize / 2;
+    }
+  });
+  return range[0];
+};
+
+const processLine = (line) => {
+  const row = binaryPartition(line.slice(0, 7), "B");
+  const col = binaryPartition(line.slice(7), "R");
+  const seat = row * 8 + col;
+  return [row, col, seat];
+};
+
+const seats = puzzleInput.map(processLine).map((r) => r[2]);
+const highestSeat = Math.max(...seats);
+
+console.log(highestSeat);
