49d010b0a317df8f1d63176a29396834ef4a7a31
Nick Pedersen <admin@nickpedersen.com>
2020-12-06 20:26:21 +1100

    Day 6

---
+++ day6-part1.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const puzzleInput = fs
+  .readFileSync(__dirname + "/day6input.txt")
+  .toString()
+  .split("\n\n");
+
+const processGroup = (groupString) => {
+  const people = groupString.split("\n");
+
+  const lettersHash = {};
+
+  people.forEach((person) => {
+    person.split("").forEach((letter) => {
+      if (!lettersHash[letter]) lettersHash[letter] = 0;
+      lettersHash[letter] += 1;
+    });
+  });
+
+  return Object.keys(lettersHash).length;
+};
+
+const yesCounts = puzzleInput.map(processGroup);
+
+const totalCount = yesCounts.reduce((prev, curr) => prev + curr, 0);
+
+console.log(totalCount);
